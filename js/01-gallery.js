16c814550ee1cf4fbf95f45c391b2befbd0c7515
Sergio Nezhigay <abc1971h@gmail.com>
2023-02-10 09:25:41 +0200

    oo

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,20 +1,32 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
 const galleryEl = document.querySelector('div.gallery');
+
+const galleryItemsHtml = galleryItems.reduce((acc, galleryItem) => {
+  return acc + galleryItemHTML(galleryItem);
+}, '');
+
+galleryEl.innerHTML = galleryItemsHtml;
+
 galleryEl.addEventListener('click', onGalleryClick);
+
+function onGalleryClick(e) {
+  e.preventDefault();
+  if (e.target.nodeName !== 'IMG') return;
+  openLightbox(e.target.dataset.source);
+}
 
 function galleryItemHTML(galleryItem) {
   return `<div class="gallery__item">
-<a class="gallery__link" href="${galleryItem.original}">
-  <img
-    class="gallery__image"
-    src="${galleryItem.preview}"
-    data-source="${galleryItem.original}"
-    alt="${galleryItem.description}"
-  />
-</a>
-</div>`;
+            <a class="gallery__link" href="${galleryItem.original}">
+              <img
+                class="gallery__image"
+                src="${galleryItem.preview}"
+                data-source="${galleryItem.original}"
+                alt="${galleryItem.description}"
+              />
+            </a>
+          </div>`;
 }
 
 function openLightbox(link) {
@@ -22,34 +34,16 @@ function openLightbox(link) {
     <img src="${link}" class="lightbox" >
 `);
   instance.show();
-  return instance;
-}
-
-function onGalleryClick(e) {
-  e.preventDefault();
-  if (e.target.nodeName !== 'IMG') return;
-
-  const instance = openLightbox(e.target.dataset.source);
-  const lightboxEl = document.querySelector('.lightbox');
-
-  if (lightboxEl) {
-    instance.close();
-    document.removeEventListener('keydown', onEscapePressed);
-  }
-  document.addEventListener('keydown', onEscapePressed);
-
-  function onEscapePressed(e) {
-    if (e.key === 'Escape') closeInstanceAndListener(instance, onEscapePressed);
+  // add on Escape key press handler
+  if (instance.visible()) {
+    document.addEventListener('keydown', onEscapePressed);
+    function onEscapePressed(e) {
+      if (e.key === 'Escape') removeLightboxAndHandler(instance, onEscapePressed);
+    }
   }
 }
 
-function closeInstanceAndListener(instance, functionToRemove) {
+function removeLightboxAndHandler(instance, functionToRemove) {
   instance.close();
   document.removeEventListener('keydown', functionToRemove);
 }
-
-const newHTML = galleryItems.reduce((acc, galleryItem) => {
-  return acc + galleryItemHTML(galleryItem);
-}, '');
-
-galleryEl.innerHTML = newHTML;
