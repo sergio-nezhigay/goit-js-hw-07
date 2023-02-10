bff991fe254e7b1a7def021cb12831e80def42cb
Sergio Nezhigay <abc1971h@gmail.com>
2023-02-10 13:20:53 +0200

    p2 start

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,49 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryEl = document.querySelector('div.gallery');
 
-console.log(galleryItems);
+const galleryItemsHtml = galleryItems.reduce((acc, galleryItem) => {
+  return acc + galleryItemHTML(galleryItem);
+}, '');
+
+galleryEl.innerHTML = galleryItemsHtml;
+
+galleryEl.addEventListener('click', onGalleryClick);
+
+function onGalleryClick(e) {
+  e.preventDefault();
+  if (e.target.nodeName !== 'IMG') return;
+  openLightbox(e.target.dataset.source);
+}
+
+function galleryItemHTML(galleryItem) {
+  return `<div class="gallery__item">
+            <a class="gallery__link" href="${galleryItem.original}">
+              <img
+                class="gallery__image"
+                src="${galleryItem.preview}"
+                data-source="${galleryItem.original}"
+                alt="${galleryItem.description}"
+              />
+            </a>
+          </div>`;
+}
+
+function openLightbox(link) {
+  const instance = basicLightbox.create(`
+    <img src="${link}" class="lightbox" >
+`);
+  instance.show();
+  // add on Escape key press handler
+  if (instance.visible()) {
+    document.addEventListener('keydown', onEscapePressed);
+    function onEscapePressed(e) {
+      if (e.key === 'Escape') removeLightboxAndHandler(instance, onEscapePressed);
+    }
+  }
+}
+
+function removeLightboxAndHandler(instance, functionToRemove) {
+  instance.close();
+  document.removeEventListener('keydown', functionToRemove);
+}
